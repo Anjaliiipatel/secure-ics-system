23e78ef22a5cfe2e71e9c48a65820564018dec7f
Your Name <you@example.com>
2026-07-04 12:56:15 -0400

    Refactor app.py to update host and port configuration for production

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from controller.controller_api import telemetry_bp
 
@@ -17,9 +18,10 @@ def health_check():
     }
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
